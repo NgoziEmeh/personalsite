330528c99f264b3b5e5b8af61a407a8051a47d7a
Ngozi Emeh <zingo1ng@yahoo.com>
2019-09-11 20:49:38 +0100

    skill javascript

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const codeSkillBtn = document.querySelector(".coding_skill_btn");
+const bizSkillBtn = document.querySelector(".biz_skill_btn");
+const codeSkillArea = document.querySelector(".coding_skills");
+const bizSkillArea = document.querySelector(".biz_skills");
+
+
+
+codeSkillBtn.addEventListener("click", () => {
+    codeSkillArea.classList.toggle("show");
+    if (codeSkillArea.style.display = "flex") {
+        bizSkillArea.style.display = "none";
+    } else {
+        bizSkillArea.style.display = "flex";
+
+    }
+});
+
+bizSkillBtn.addEventListener("click", () => {
+    bizSkillArea.classList.toggle("show");
+    if (bizSkillArea.style.display = "flex") {
+        codeSkillArea.style.display = "none";
+    } else {
+        codeSkillArea.style.display = "flex"
+
+
+    }
+
+});
