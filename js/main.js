088f679fec29b08a48297430a5f738e32cd6b65b
Ngozi Emeh <zingo1ng@yahoo.com>
2019-09-15 23:08:14 +0100

    school updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// Section: Navigation
+const ham = document.querySelector(".hamburger");
+const nav = document.querySelector(".navigations");
+
+ham.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
+// Section: Education
+const schBtn = document.querySelector(".school");
+const streetBtn = document.querySelector(".informal_edu");
+
+const streetDetail = document.querySelector(".street_detail");
+const schDetail = document.querySelector(".school_detail");
+
+schBtn.addEventListener("click", () => {
+    schDetail.classList.toggle("hide");
+    if (schDetail.style.display = "block") {
+        streetDetail.style.display = "none";
+    } else {
+        streetDetail.style.display = "block";
+    };
+});
+
+streetBtn.addEventListener("click", () => {
+    streetDetail.classList.toggle("hide");
+    if (streetDetail.style.display = "block") {
+        schDetail.style.display = "none";
+    } else {
+        schDetail.style.display = "block";
+    };
+});
+
 // Page: skills
 // Section: skill btn
 const codeSkillBtn = document.querySelector(".coding_skill_btn");
@@ -27,28 +60,4 @@ bizSkillBtn.addEventListener("click", () => {
 
     }
 
-});
-
-// Section: Navigation
-const ham = document.querySelector(".hamburger");
-const nav = document.querySelector(".navigations");
-
-ham.addEventListener("click", () => {
-    nav.classList.toggle("open");
-});
-
-
-// Section: Education
-const schBtn = document.querySelector(".school");
-const streetBtn = document.querySelector(".informal_edu");
-
-const streetDetail = document.querySelector(".street_detail");
-const schDetail = document.querySelector(".school_detail");
-
-schBtn.addEventListener("click", () => {
-    schDetail.classList.toggle("hide");
-});
-
-streetBtn.addEventListener("click", () => {
-    streetDetail.classList.toggle("hide");
 });
