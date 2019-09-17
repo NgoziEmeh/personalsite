705f2f7a2637d885c8ca73c55cc1fae054f0667f
Ngozi Emeh <zingo1ng@yahoo.com>
2019-09-17 16:12:55 +0100

    all responsive

--- js/edu.js
+++ js/edu.js
@@ -1,24 +1,56 @@
+const codeSkillBtn = document.querySelector(".coding_skill_btn");
+const bizSkillBtn = document.querySelector(".biz_skill_btn");
+const codeSkillArea = document.querySelector(".coding_skills");
+const bizSkillArea = document.querySelector(".biz_skills");
+
+
+
+codeSkillBtn.addEventListener("click", () => {
+    codeSkillArea.classList.toggle("show");
+    if (codeSkillArea.style.display = "flex") {
+        bizSkillArea.style.display = "none";
+    } else {
+        bizSkillArea.style.display = "block";
+
+    }
+});
+
+bizSkillBtn.addEventListener("click", () => {
+    bizSkillArea.classList.toggle("show");
+    if (bizSkillArea.style.display = "block") {
+        codeSkillArea.style.display = "none";
+    } else {
+        codeSkillArea.style.display = "flex"
+
+
+    }
+
+});
+
+
+
+
 // Section: Education
-const schBtn = document.querySelector(".school");
-const streetBtn = document.querySelector(".informal_edu");
+// const schBtn = document.querySelector(".school");
+// const streetBtn = document.querySelector(".informal_edu");
 
-const streetDetail = document.querySelector(".street_detail");
-const schDetail = document.querySelector(".school_detail");
+// const streetDetail = document.querySelector(".street_detail");
+// const schDetail = document.querySelector(".school_detail");
 
-schBtn.addEventListener("click", () => {
-    schDetail.classList.toggle("hide");
-    if (schDetail.style.display = "block") {
-        streetDetail.style.display = "none";
-    } else {
-        streetDetail.style.display = "block";
-    }
-});
+// schBtn.addEventListener("click", () => {
+//     schDetail.classList.toggle("hide");
+//     if (schDetail.style.display = "block") {
+//         streetDetail.style.display = "none";
+//     } else {
+//         streetDetail.style.display = "block";
+//     }
+// });
 
-streetBtn.addEventListener("click", () => {
-    streetDetail.classList.toggle("hide");
-    if (streetDetail.style.display = "block") {
-        schDetail.style.display = "none";
-    } else {
-        schDetail.style.display = "block"
-    }
-});
+// streetBtn.addEventListener("click", () => {
+//     streetDetail.classList.toggle("hide");
+//     if (streetDetail.style.display = "block") {
+//         schDetail.style.display = "none";
+//     } else {
+//         schDetail.style.display = "block"
+//     }
+// });
--- js/main.js
+++ js/main.js
@@ -33,31 +33,31 @@ streetBtn.addEventListener("click", () => {
 
 // Page: skills
 // Section: skill btn
-const codeSkillBtn = document.querySelector(".coding_skill_btn");
-const bizSkillBtn = document.querySelector(".biz_skill_btn");
-const codeSkillArea = document.querySelector(".coding_skills");
-const bizSkillArea = document.querySelector(".biz_skills");
+// const codeSkillBtn = document.querySelector(".coding_skill_btn");
+// const bizSkillBtn = document.querySelector(".biz_skill_btn");
+// const codeSkillArea = document.querySelector(".coding_skills");
+// const bizSkillArea = document.querySelector(".biz_skills");
 
 
 
-codeSkillBtn.addEventListener("click", () => {
-    codeSkillArea.classList.toggle("show");
-    if (codeSkillArea.style.display = "flex") {
-        bizSkillArea.style.display = "none";
-    } else {
-        bizSkillArea.style.display = "block";
+// codeSkillBtn.addEventListener("click", () => {
+//     codeSkillArea.classList.toggle("show");
+//     if (codeSkillArea.style.display = "flex") {
+//         bizSkillArea.style.display = "none";
+//     } else {
+//         bizSkillArea.style.display = "block";
 
-    }
-});
+//     }
+// });
 
-bizSkillBtn.addEventListener("click", () => {
-    bizSkillArea.classList.toggle("show");
-    if (bizSkillArea.style.display = "block") {
-        codeSkillArea.style.display = "none";
-    } else {
-        codeSkillArea.style.display = "flex"
+// bizSkillBtn.addEventListener("click", () => {
+//     bizSkillArea.classList.toggle("show");
+//     if (bizSkillArea.style.display = "block") {
+//         codeSkillArea.style.display = "none";
+//     } else {
+//         codeSkillArea.style.display = "flex"
 
 
-    }
+//     }
 
-});
+// });
